7edd4b7603719c8b6a0c297e3d1cee30d10dd440
soleiana <anna2111@gmai.com>
2017-03-21 22:28:20 +0200

    application context test

---
+++ src/test/java/com/demo/ApplicationContextTest.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+import com.demo.core.RawPhoneNumberFactory;
+import lombok.extern.java.Log;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@Log
+@Component
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ApplicationContextTest {
+
+	@Autowired
+	private ApplicationContext context;
+
+		@Test
+	public void contextLoads() {
+		log.info("context loaded...");
+		RawPhoneNumberFactory factory = context.getBean(RawPhoneNumberFactory.class);
+		assertNotNull(factory);
+	}
+
+}
